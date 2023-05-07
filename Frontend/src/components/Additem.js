c309cfd2b41f270e925008dbff6e51b6c195b102
MiHi <mrmihiraj@gmail.com>
2023-05-07 17:22:15 +0530

    Added Image Upload

--- Frontend/src/components/Additem.js
+++ Frontend/src/components/Additem.js
@@ -1,112 +1,166 @@
-import { Box, FormLabel, TextField, } from '@mui/material'
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Button from '@mui/material/Button';
+import { Box, FormLabel, TextField } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Button from "@mui/material/Button";
 
-import "./Additem.css"
-import React, { useState } from 'react'
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import "./Additem.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const Add = () => {
   const history = useNavigate();
   const [inputs, setInputs] = useState({
-    name: '',
-    description: '',
-    price: '',
-    catagory: '',
+    name: "",
+    description: "",
+    price: "",
+    catagory: "",
     available: false,
-    image: '',
-
-
+    image: "",
   });
   const [checked, setChecked] = useState(false);
   const handleChange = (e) => {
     setInputs((prevState) => ({
       ...prevState,
       [e.target.name]: e.target.value,
-
-    }))
+    }));
   };
   const sendRequest = async () => {
-   await axios.post('http://localhost:5000/books', {
-      name:String( inputs.name),
-      author: String(inputs.catagory),
-      description: String(inputs.description),
-      price: Number(inputs.price),
-      image: String(inputs.image),
-      available: Boolean(checked),
-    }).then(res=> res.data);
-  }
-
+    await axios
+      .post("http://localhost:5000/books", {
+        name: String(inputs.name),
+        author: String(inputs.catagory),
+        description: String(inputs.description),
+        price: Number(inputs.price),
+        image: String(inputs.image),
+        available: Boolean(checked),
+      })
+      .then((res) => res.data);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(inputs,checked);
-    sendRequest().then(()=>history('/products'))
-  }
+    console.log(inputs, checked);
+    sendRequest().then(() => history("/products"));
+  };
+
+  const [imageSelected, setImageSelected] = useState("");
+
+  const uploadImage = () => {
+    if (imageSelected) {
+      const formData = new FormData();
+      formData.append("file", imageSelected);
+      formData.append("upload_preset", "fxqr8qoz");
+
+      axios
+        .post(
+          "https://api.cloudinary.com/v1_1/dpfeb0kt9/image/upload",
+          formData
+        )
+        .then((response) => {
+          console.log(response);
+          const imageUrl = response.data.secure_url;
+          console.log("Cloud URL " + imageUrl);
+          setInputs({
+            ...inputs,
+            image: imageUrl,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    uploadImage();
+  }, [imageSelected]);
+
   return (
     <form onSubmit={handleSubmit}>
-      <Box display="flex" 
-      flexDirection="column" 
-      justifyContent={'center'}
-       maxWidth={700}
-       alignContent={"center"}
-       alignSelf="center"
-       marginLeft={"auto"}
-       marginRight="auto"
-       marginTop={10}
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent={"center"}
+        maxWidth={700}
+        alignContent={"center"}
+        alignSelf="center"
+        marginLeft={"auto"}
+        marginRight="auto"
+        marginTop={10}
+      >
+        <FormLabel>Name</FormLabel>
+        <TextField
+          value={inputs.name}
+          onChange={handleChange}
+          margin="normal"
+          l
+          abel="Name"
+          variant="outlined"
+          name="name"
+        />
+        <FormLabel>Catagory</FormLabel>
+        <TextField
+          value={inputs.catagory}
+          onChange={handleChange}
+          margin="normal"
+          label="Catagory"
+          variant="outlined"
+          name="catagory"
+        />
+        <FormLabel>Description</FormLabel>
+        <TextField
+          value={inputs.description}
+          onChange={handleChange}
+          margin="normal"
+          label="Description"
+          variant="outlined"
+          name="description"
+        />
+        <FormLabel>price</FormLabel>
+        <TextField
+          value={inputs.price}
+          onChange={handleChange}
+          type="number"
+          margin="normal"
+          label="Price"
+          variant="outlined"
+          name="price"
+        />
 
-       >
-      <FormLabel>Name</FormLabel>
-      <TextField value={inputs.name} 
-      onChange={handleChange} 
-      margin='normal' l
-      abel='Name' 
-      variant='outlined' name='name' />
-      <FormLabel>Catagory</FormLabel>
-      <TextField 
-      value={inputs.catagory} 
-      onChange={handleChange}
-      margin='normal' 
-      label='Catagory' 
-      variant='outlined' 
-      name='catagory' />
-      <FormLabel>Description</FormLabel>
-      <TextField 
-      value={inputs.description} 
-      onChange={handleChange} 
-      margin='normal'
-       label='Description' 
-       variant='outlined' 
-       name='description' />
-      <FormLabel>price</FormLabel>
-      <TextField 
-      value={inputs.price}
-       onChange={handleChange}
-      type='number'
-      margin='normal' label='Price' variant='outlined' name='price' />
-      <FormLabel>Image</FormLabel>
+        <FormLabel>Upload Event Image (Max Size: 5MB)</FormLabel>
 
-      <TextField 
-      value={inputs.image} onChange={handleChange}
-      margin='normal' variant='outlined' name='image' />
+        <TextField
+          key="image"
+          name="image"
+          type="file"
+          onChange={(e) => {
+            console.log(e.target.files[0]);
+            setImageSelected(e.target.files[0]);
+            uploadImage();
+            setInputs({ ...inputs, [e.target.name]: e.target.value });
+            console.log();
+          }}
+        />
 
-{/* <Button variant="contained" component="label" value={inputs.image} onChange={handleChange}>
+        {/* <Button variant="contained" component="label" value={inputs.image} onChange={handleChange}>
   Upload
   <input hidden accept="image/*" multiple type="file" />
 </Button> */}
 
-      <FormControlLabel
-      control={<Checkbox checked={checked} onChange={()=>setChecked(!checked)}/>} 
-      label="Available" />
+        <FormControlLabel
+          control={
+            <Checkbox checked={checked} onChange={() => setChecked(!checked)} />
+          }
+          label="Available"
+        />
 
-
-      <button class = "button" variant="contained" type='submit'>Add Product</button>
+        <button class="button" variant="contained" type="submit">
+          Add Product
+        </button>
       </Box>
-    
-      </form>
-  )
-}
+    </form>
+  );
+};
 
-export default Add
+export default Add;
--- Frontend/src/components/Product/Itemdetail.js
+++ Frontend/src/components/Product/Itemdetail.js
@@ -1,106 +1,159 @@
-import { Box, FormLabel, TextField, } from '@mui/material'
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { useNavigate, useParams } from 'react-router-dom';
+import { Box, FormLabel, TextField } from "@mui/material";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Itemdetail = () => {
   const [inputs, setInputs] = useState({});
-    const id = useParams().id;
-    const [checked, setChecked] = useState(false);
-    const  history = useNavigate();
-    useEffect(() => { 
-        const fetchHandler = async() => {
-          await  axios
-          .get(`http://localhost:5000/books/${id}`)
-          .then((res) => res.data).then(data=>setInputs(data.book ));
-
-        };
-        fetchHandler().then((data) => setInputs(data.book));
-    }, [id]);
-
-    const sendRequest = async () => {
-      await axios.put(`http://localhost:5000/books/${id}`,{
-        name:String( inputs.name),
-      author: String(inputs.author),
-      description: String(inputs.description),
-      price: Number(inputs.price),
-      image: String(inputs.image),
-      available: Boolean(checked),
-      }).then(res=> res.data);
-    }
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        sendRequest().then(()=>history("/products"));
-
-    }
-    const handleChange = (e) => {
-      setInputs((prevState) => ({
-        ...prevState,
-        [e.target.name]: e.target.value,
-  
-      }))
-      
+  const id = useParams().id;
+  const [checked, setChecked] = useState(false);
+  const history = useNavigate();
+  useEffect(() => {
+    const fetchHandler = async () => {
+      await axios
+        .get(`http://localhost:5000/books/${id}`)
+        .then((res) => res.data)
+        .then((data) => setInputs(data.book));
     };
-    
+    fetchHandler().then((data) => setInputs(data.book));
+  }, [id]);
 
+  const sendRequest = async () => {
+    await axios
+      .put(`http://localhost:5000/books/${id}`, {
+        name: String(inputs.name),
+        author: String(inputs.author),
+        description: String(inputs.description),
+        price: Number(inputs.price),
+        image: String(inputs.image),
+        available: Boolean(checked),
+      })
+      .then((res) => res.data);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    sendRequest().then(() => history("/products"));
+  };
+  const handleChange = (e) => {
+    setInputs((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const [imageSelected, setImageSelected] = useState("");
+
+  const uploadImage = () => {
+    if (imageSelected) {
+      const formData = new FormData();
+      formData.append("file", imageSelected);
+      formData.append("upload_preset", "fxqr8qoz");
+
+      axios
+        .post(
+          "https://api.cloudinary.com/v1_1/dpfeb0kt9/image/upload",
+          formData
+        )
+        .then((response) => {
+          console.log(response);
+          const imageUrl = response.data.secure_url;
+          console.log("Cloud URL " + imageUrl);
+          setInputs({
+            ...inputs,
+            image: imageUrl,
+          });
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+  };
+
+  useEffect(() => {
+    uploadImage();
+  }, [imageSelected]);
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <Box display="flex" 
-      flexDirection="column" 
-      justifyContent={'center'}
-       maxWidth={700}
-       alignContent={"center"}
-       alignSelf="center"
-       marginLeft={"auto"}
-       marginRight="auto"
-       marginTop={10}
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent={"center"}
+          maxWidth={700}
+          alignContent={"center"}
+          alignSelf="center"
+          marginLeft={"auto"}
+          marginRight="auto"
+          marginTop={10}
+        >
+          <FormLabel>Name</FormLabel>
+          <TextField
+            value={inputs.name}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            name="name"
+          />
+          <FormLabel>Catagory</FormLabel>
+          <TextField
+            value={inputs.author}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            name="catagory"
+          />
+          <FormLabel>description</FormLabel>
+          <TextField
+            value={inputs.description}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            name="description"
+          />
+          <FormLabel>price</FormLabel>
+          <TextField
+            value={inputs.price}
+            onChange={handleChange}
+            type="number"
+            margin="normal"
+            variant="outlined"
+            name="price"
+          />
+          <FormLabel>Image</FormLabel>
 
-       >
-      <FormLabel>Name</FormLabel>
-      <TextField value={inputs.name} 
-      onChange={handleChange} 
-      margin='normal'  
-      variant='outlined' 
-      name='name' />
-      <FormLabel>Catagory</FormLabel>
-      <TextField 
-      value={inputs.author} 
-      onChange={handleChange}
-      margin='normal' 
-      variant='outlined' 
-      name='catagory' />
-      <FormLabel>description</FormLabel>
-      <TextField 
-      value={inputs.description} 
-      onChange={handleChange} 
-      margin='normal'
-       variant='outlined' 
-       name='description' />
-      <FormLabel>price</FormLabel>
-      <TextField 
-      value={inputs.price}
-       onChange={handleChange}
-      type='number'
-      margin='normal'  variant='outlined' name='price' />
-      <FormLabel>Image</FormLabel>
-      <TextField 
-      value={inputs.image} onChange={handleChange}
-      margin='normal' variant='outlined' name='image' />
-      <FormControlLabel
-      control={<Checkbox checked={checked} onChange={()=>setChecked(!checked)}/>} 
-      label="Available" />
+          <TextField
+            key="image"
+            name="image"
+            type="file"
+            onChange={(e) => {
+              console.log(e.target.files[0]);
+              setImageSelected(e.target.files[0]);
+              uploadImage();
+              setInputs({ ...inputs, [e.target.name]: e.target.value });
+              console.log();
+            }}
+          />
 
+          <FormControlLabel
+            control={
+              <Checkbox
+                checked={checked}
+                onChange={() => setChecked(!checked)}
+              />
+            }
+            label="Available"
+          />
 
-      <button variant="contained" type='submit'>Update Product</button>
-      </Box>
-    
+          <button variant="contained" type="submit">
+            Update Product
+          </button>
+        </Box>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Itemdetail
+export default Itemdetail;
